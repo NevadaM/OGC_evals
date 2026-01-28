3e6f8860dbcc30deaad3092092fda26c323f730f
Neil laptop <neil.majithia@live.co.uk>
2026-01-28 10:11:22 +0000

    testing_changes

--- testing/testing_generator.py
+++ testing/testing_generator.py
@@ -17,7 +17,7 @@ from ogc_eval.model import LLMWrapper
 
 csv_path = os.path.join(os.path.dirname(__file__), '..', 'public_set_1.csv')
 API_KEY = os.environ['GEMINI_API_KEY']
-MODEL_UNDER_TEST = 'gemini/gemini-3-flash-preview'
+MODEL_UNDER_TEST = 'gemini/gemini-2.5-flash'
 
 model = LLMWrapper(model_name=MODEL_UNDER_TEST, api_key=API_KEY)
 
@@ -26,4 +26,4 @@ generator = Generator_Model(model=model, context='zero')
 
 results = generator.run(genLoader)
 
-genLoader.save(results, label=MODEL_UNDER_TEST)
+genLoader.save(results, label=f'{MODEL_UNDER_TEST}_0SHOT')
